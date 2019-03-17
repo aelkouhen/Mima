4b1f70c31ee1df7f79320d8f689475d21118cfb4
aelkouhen <Arigatou86*>
2019-03-17 17:24:36 +0100

    register to eureka discovery server

--- src/main/java/com/carhub/api/Application.java
+++ src/main/java/com/carhub/api/Application.java
@@ -3,6 +3,7 @@ package com.carhub.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.carhub.api.auth.repositories", repositoryFactoryBeanClass = JpaRepositoryFactoryBean.class)
 public class Application {
 	public static void main(String[] args) {
